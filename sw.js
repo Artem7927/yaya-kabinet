82bd52e42f649c8766b24aeac73f3a63b10a9a75
Artem7927 <artem27metra@gmail.com>
2026-08-08 20:07:41 +0500

    fix(kitchen-stock): превью готовки/калькуляторы берут остатки с сервера (availAll), фолбэк на локаль

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v4';
+const CACHE = 'yaya-v5';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
