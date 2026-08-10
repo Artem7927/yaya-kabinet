420128a2f8657fb700653d428b41664b10dba210
Artem7927 <artem27metra@gmail.com>
2026-08-10 21:04:52 +0500

    chore(sw): bump cache v12 для сброса старого рендера техкарт

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v11';
+const CACHE = 'yaya-v12';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
