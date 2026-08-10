6626b6486844b034260ae1b77f50b560e21dfcad
Artem7927 <artem27metra@gmail.com>
2026-08-10 23:30:13 +0500

    ui(buyer): фото в журнале 2-в-ряд + инфо снизу, sw v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v16';
+const CACHE = 'yaya-v17';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
