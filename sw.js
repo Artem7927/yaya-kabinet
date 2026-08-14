c18aefa4b7a02269f1311d1c12bc5d5e946cc9b8
Artem7927 <artem27metra@gmail.com>
2026-08-14 17:06:48 +0500

    списание: премиальная кнопка фото (цех/кухня) как у закупщика; sw v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v26';
+const CACHE = 'yaya-v27';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
