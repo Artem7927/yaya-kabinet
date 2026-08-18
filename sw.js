74febca197df8d9fa6ce2213ba38453835492564
Artem7927 <artem27metra@gmail.com>
2026-08-18 19:51:06 +0500

    feat(buyer-mobile): show deliver button for 🚚/🛍/🧾 types with SVG icons, prefill price from assign sum for 🧾

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v34';
+const CACHE = 'yaya-v35';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
