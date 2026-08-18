1cf87cc02217d51a5c518ac1b797b4ab08850332
Artem7927 <artem27metra@gmail.com>
2026-08-18 19:04:35 +0500

    feat(manager-mobile): add 🛍 zakupshik type + SVG icons with labels for all assign types

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v33';
+const CACHE = 'yaya-v34';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
