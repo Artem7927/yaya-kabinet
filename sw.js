1a4f1c2581146089cd5ae6fc52ca273e70eb78bf
Artem7927 <artem27metra@gmail.com>
2026-08-26 02:53:17 +0500

    fix(buyer): фильтр дат в «Купить» виден всегда (кнопка Сегодня + прошлые дни); sw v54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v53';
+const CACHE = 'yaya-v54';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
