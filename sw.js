132f27ffb347d4e03280fab6df5d5954cad8c6b9
Artem7927 <artem27metra@gmail.com>
2026-08-26 19:22:46 +0500

    feat(kitchen): cook() авто-создаёт заявку в цех на списанные ПФ (POST /pf-requests qty=списанное); sw v61

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v60';
+const CACHE = 'yaya-v61';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
