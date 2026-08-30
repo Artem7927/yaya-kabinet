b30712957ce0609eaa457542134dad01448441b4
Artem7927 <artem27metra@gmail.com>
2026-08-30 06:39:28 +0500

    feat(mobile): SW-авто-апдейт безопасной перезагрузкой всем мобильным + авто-рефетч 15с для закупщика; sw v66

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v65';
+const CACHE = 'yaya-v66';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
