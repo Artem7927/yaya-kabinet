a6c9ac53bc8f54b7122d924b18a3062b0d240701
Artem7927 <artem27metra@gmail.com>
2026-08-31 14:44:14 +0500

    fix(kitchen): честный сигнал 'нет доступа к складу' при пустых данных (не ложное 'мало'); cook() toast 'Недостаточно: <имя>' через NAME_BY_ID; sw v71

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v70';
+const CACHE = 'yaya-v71';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
