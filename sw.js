cfb4e95797a13c36ea71bd9ec92d15409bcc4c90
Artem7927 <artem27metra@gmail.com>
2026-09-04 02:28:28 +0500

    kitchen: unify into single tabbed Orders screen, remove kitchen.html, fix tasks, sw v74

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v73';
+const CACHE = 'yaya-v74';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
