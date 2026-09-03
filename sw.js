dfec2db314f55e6226a7b9f1d16a0e55dd303bd7
Artem7927 <artem27metra@gmail.com>
2026-09-04 01:00:38 +0500

    kitchen: single server-sourced screen, tabs+counters, no emoji, sw v73

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v72';
+const CACHE = 'yaya-v73';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
