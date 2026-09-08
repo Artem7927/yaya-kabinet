2836b1150a27dc903c67bd65bfaf0ad2a6cd665b
Artem7927 <artem27metra@gmail.com>
2026-09-08 15:19:24 +0500

    manager: date-period filter on orders list, drop 1970 junk, ts on order create, fix _ordRange decl; sw v84

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v83';
+const CACHE = 'yaya-v84';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
