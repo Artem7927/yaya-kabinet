8f825b2c6310cbb64367ff817e1aecdc9e8461ca
Artem7927 <artem27metra@gmail.com>
2026-09-10 22:07:45 +0500

    manager: live orders polling (fresh on top, no reverse) + receipt from order data on all cabinets; sw v85

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yaya-v84';
+const CACHE = 'yaya-v85';
 const API_HOST = 'yaya-db-production.up.railway.app';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
